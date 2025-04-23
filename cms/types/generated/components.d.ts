c224842eb4732129897719d0372c982bbd57f5b9
Liam Hjalmarsson <Liam.hjalmarsson@hotmail.com>
2025-04-23 13:49:39 +0200

    Component Link strapi

--- cms/types/generated/components.d.ts
+++ cms/types/generated/components.d.ts
@@ -1,20 +1,40 @@
 import type { Schema, Struct } from '@strapi/strapi';
 
+export interface ElementsLink extends Struct.ComponentSchema {
+  collectionName: 'components_elements_links';
+  info: {
+    displayName: 'Link';
+    icon: 'arrowRight';
+  };
+  attributes: {
+    href: Schema.Attribute.String & Schema.Attribute.Required;
+    isExternal: Schema.Attribute.Boolean & Schema.Attribute.DefaultTo<false>;
+    label: Schema.Attribute.String & Schema.Attribute.Required;
+  };
+}
+
 export interface SeoSeo extends Struct.ComponentSchema {
   collectionName: 'components_seo_seos';
   info: {
+    description: '';
     displayName: 'Seo';
     icon: 'bulletList';
   };
   attributes: {
-    description: Schema.Attribute.Text & Schema.Attribute.Required;
-    title: Schema.Attribute.String & Schema.Attribute.Required;
+    metaCanonicalURL: Schema.Attribute.String & Schema.Attribute.Required;
+    metaDescription: Schema.Attribute.Text & Schema.Attribute.Required;
+    metaImage: Schema.Attribute.Media<'images' | 'files'> &
+      Schema.Attribute.Required;
+    metaTitle: Schema.Attribute.String & Schema.Attribute.Required;
+    preventIndexing: Schema.Attribute.Boolean &
+      Schema.Attribute.DefaultTo<false>;
   };
 }
 
 declare module '@strapi/strapi' {
   export module Public {
     export interface ComponentSchemas {
+      'elements.link': ElementsLink;
       'seo.seo': SeoSeo;
     }
   }
